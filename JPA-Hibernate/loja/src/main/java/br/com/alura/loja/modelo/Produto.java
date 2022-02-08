a8153116391671e6610a7aa67375a6de479e9625
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2022-02-08 12:42:06 -0300

    Mapeando entidades

--- JPA-Hibernate/loja/src/main/java/br/com/alura/loja/modelo/Produto.java
+++ JPA-Hibernate/loja/src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -1,12 +1,16 @@
 package br.com.alura.loja.modelo;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 // import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +24,18 @@ public class Produto {
     // @Column(name="desc")
     private String descricao;
     private BigDecimal preco;
+    private LocalDate dataCadastro = LocalDate.now();
+    // @Enumerated(EnumType.STRING)
+    @ManyToOne
+    private Categoria categoria;
+
+    
+    public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
     public Long getId() {
         return id;
     }
@@ -44,7 +60,18 @@ public class Produto {
     public void setPreco(BigDecimal preco) {
         this.preco = preco;
     }
-
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+    public Categoria getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
     
 
 }
