1968e328566c46988dec930910ae08ae0937fdbb
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2022-02-21 12:15:25 -0300

    Operações CRUD

--- Spring-Data-JPA/spring-data/src/main/java/br/com/alura/spring/data/Service/CrudCargoService.java
+++ Spring-Data-JPA/spring-data/src/main/java/br/com/alura/spring/data/Service/CrudCargoService.java
@@ -10,6 +10,7 @@ import br.com.alura.spring.data.repository.CargoRepository;
 @Service
 public class CrudCargoService {
 
+    private Boolean system = true;
     private final CargoRepository repository;
 
     public CrudCargoService(CargoRepository repository) {
@@ -18,7 +19,35 @@ public class CrudCargoService {
 
 
     public void iniciar(Scanner scanner) {
-        salvar(scanner);
+
+        while(system) {
+            System.out.println("Qual ação deseja realizar?\n"
+                    + "0 - Sair\n"
+                    + "1 - Salvar\n"
+                    + "2 - Atualizar\n"
+                    + "3 - Visualizar\n"
+                    + "4 - Deletar\n"
+                    + "Opção: ");
+            int action = scanner.nextInt();
+
+            switch (action) {
+                case 1:
+                    salvar(scanner);
+                    break;
+                case 2:
+                    atualizar(scanner);
+                    break;
+                case 3:
+                    visualizar();
+                    break;
+                case 4:
+                    deletar(scanner);
+                    break;
+                default:
+                    system = false;
+                    break;
+            }
+        }
     }
 
     public void salvar(Scanner scanner) {
@@ -33,6 +62,33 @@ public class CrudCargoService {
         System.out.println("Salvo");
     }
 
+    public void atualizar(Scanner scanner) {
+        int id = 0;
+        String descricao = new String();
+        Cargo cargo = new Cargo();
+
+        System.out.println("Digite o ID do Cargo que deseja alterar: ");
+        id = scanner.nextInt();
+        System.out.println("Digite a nova descrição do Cargo: ");
+        descricao = scanner.next();
+
+        cargo.setId(id);
+        cargo.setDescricao(descricao);
+        repository.save(cargo);
+        System.out.println("Atualizado");
+    }
+
+    private void visualizar(){
+        repository.findAll().forEach(System.out::println);
+    }
+
+    private void deletar(Scanner scanner){ 
+        int id = 0;
+        System.out.println("Digite o ID do Cargo que deseja deletar: ");
+        id = scanner.nextInt();
+        repository.deleteById(id);
+        System.out.println("Deletado");
+    }
 
 
 }
--- Spring-Data-JPA/spring-data/src/main/java/br/com/alura/spring/data/orm/Cargo.java
+++ Spring-Data-JPA/spring-data/src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -13,12 +13,26 @@ public class Cargo {
     private Integer id;
     private String descricao;
 
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getDescricao() {
         return descricao;
     }
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Cargo [descricao=" + descricao + ", id=" + id + "]";
     }
 
     
